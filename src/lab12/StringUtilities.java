921de729a20b9ee8ee5ce4bf2d50134cd5cdc998
jrstan17 <jrstan17@jrstan17-Nbk>
2015-05-04 22:03:41 -0400

    Initial commit

---
+++ src/lab12/StringUtilities.java
@@ -0,0 +1,25 @@
+package lab12;
+
+public class StringUtilities {
+
+    public static String toProperNoun(String str) {
+	String stringToReturn = "";
+
+	if (str != null && !str.isEmpty()) {
+
+	    stringToReturn = str.substring(0, 1).toUpperCase();
+
+	    for (int i = 1; i < str.length(); i++) {
+		if (str.charAt(i) == ' ') {
+		    stringToReturn = stringToReturn + " "
+			    + str.substring(i + 1, i + 2).toUpperCase();
+		    i++;
+		} else {
+		    stringToReturn = stringToReturn + str.charAt(i);
+		}
+	    }
+	}
+
+	return stringToReturn;
+    }
+}
